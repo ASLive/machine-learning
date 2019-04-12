4c1e483f694ae0946c30b0404fe9b8f2d15e7620
anvitamishra <anvitamishra309@gmail.com>
2019-04-12 13:46:31 -0400

    Update metric calculations and add confusion matrix

--- main.py
+++ main.py
@@ -7,21 +7,6 @@ from sklearn.metrics import *
 
 print("Tensorflow version "+tf.__version__)
 
-
-def calculate_metrics(predicted, actual):
-    TP = np.count_nonzero(predicted * actual)
-    TN = np.count_nonzero((predicted - 1) * (actual - 1))
-    FP = np.count_nonzero(predicted * (actual - 1))
-    FN = np.count_nonzero((predicted - 1) * actual)
-
-    accuracy = (TP + TN)/ (TP + FP + FN + TN)
-    precision = TP / (TP + FP)
-    recall = TP / (TP + FN)
-    f1 = 2 * precision * recall / (precision + recall)
-
-    return accuracy, precision, recall, f1
-
-
 if __name__ == "__main__":
     """
        python3 main.py # load model files if available, else retrain and save
@@ -47,18 +32,18 @@ if __name__ == "__main__":
     actual_train = train_labels
     actual_test = test_labels
 
-    # accuracy_train = accuracy_score(actual_train, pred_train)
-    # precision_train = precision_score(actual_train, pred_train, average='micro')
-    # recall_train = recall_score(actual_train, pred_train, average='micro')
-    # f1_train = f1_score(actual_train, pred_train, average='micro')
-    #
-    # accuracy_test = accuracy_score(actual_test, pred_test)
-    # precision_test = precision_score(actual_test, pred_test, average='micro')
-    # recall_test = recall_score(actual_test, pred_test, average='micro')
-    # f1_test = f1_score(actual_test, pred_test, average='micro')
+    cm_train = confusion_matrix(y_true=actual_train, y_pred=pred_train)
+    cm_test = confusion_matrix(y_true=actual_test, y_pred=pred_test)
 
-    accuracy_train, precision_train, recall_train, f1_train = calculate_metrics(pred_train, actual_train)
-    accuracy_test, precision_test, recall_test, f1_test = calculate_metrics(pred_test, actual_test)
+    accuracy_train = accuracy_score(actual_train, pred_train)
+    precision_train = precision_score(actual_train, pred_train, average='micro')
+    recall_train = recall_score(actual_train, pred_train, average='micro')
+    f1_train = f1_score(actual_train, pred_train, average='micro')
+
+    accuracy_test = accuracy_score(actual_test, pred_test)
+    precision_test = precision_score(actual_test, pred_test, average='micro')
+    recall_test = recall_score(actual_test, pred_test, average='micro')
+    f1_test = f1_score(actual_test, pred_test, average='micro')
 
     print('\nTraining accuracy:\t' + str(accuracy_train))
     print('Training precision:\t' + str(precision_train))
@@ -69,6 +54,9 @@ if __name__ == "__main__":
     print('Testing precision:\t' + str(precision_test))
     print('Testing recall:\t' + str(recall_test))
     print('Testing f1:\t' + str(f1_test))
+
+    print('\nTraining Confusion Matrix:\n' + str(cm_train))
+    print('\nTesting Confusion Matrix:\n' + str(cm_test))
 
     if visualize_arg:
         # display test predictions
