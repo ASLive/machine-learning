449ad0c33214cc506f4a10bfad1bfda7af534bdf
John Doe <johndoe@example.com>
2019-05-20 18:49:03 -0400

    added conv2d

--- data.py
+++ data.py
@@ -1,19 +1,60 @@
 import os
 import numpy as np
 import math
+import scipy
 import pickle
+
+TRAIN_DATA_PATH = "./data"
 
 def load_data():
     images, labels, class_names = read_data()
+    # return split_data(images, labels)
     (train_images, train_labels), (test_images, test_labels) = split_data(images, labels)
-    return [train_images, train_labels, test_images, test_labels, class_names]
+    return (train_images, train_labels), (test_images, test_labels), class_names
+    # return [train_images, train_labels, test_images, test_labels, class_names]
+
+def rgb2gray(rgb):
+    r, g, b = rgb[:,:,0], rgb[:,:,1], rgb[:,:,2]
+    gray = 0.3333 * r + 0.3333 * g + 0.3333 * b
+    return gray
+
+def read_hand3d():
+    """read data from files (run setup_asl.py to generate)"""
+    images = pickle.load( open("images.pickle","rb") )
+    labels = pickle.load( open("labels.pickle","rb") )
+    classes = pickle.load( open("classes.pickle","rb") )
+    return np.array(images), np.array(labels), classes
 
 def read_data():
-    """read data from files (run setup_asl.py to generate)"""
-    images = pickle.load( open("pickle/images.pickle","rb") )
-    labels = pickle.load( open("pickle/labels.pickle","rb") )
-    classes = pickle.load( open("pickle/classes.pickle","rb") )
-    return np.array(images), np.array(labels), classes
+    """read data from files"""
+    print("loading data...",end="")
+    ret_images = []
+    ret_labels = []
+    ret_class_names = []
+    count = 0
+    for label in list(os.walk(TRAIN_DATA_PATH)): # walk directory
+        full_path, image_list = label[0], label[2]
+        letter = full_path[len(TRAIN_DATA_PATH)+1:] # get letter class
+        if len(letter) > 0:
+            # get list of file paths to each image
+            image_path_list = [TRAIN_DATA_PATH+"/"+letter+"/"+file for file in image_list]
+            ret_class_names.append(count)
+            # print(letter, count)
+            print(".",end="")
+            count += 1
+            if len(image_path_list) > 0:
+                # iterate each image
+                for i in range(len(image_path_list)):
+                    # add image, letter to ret array
+                    image = scipy.misc.imread(image_path_list[i])
+                    # image = scipy.misc.imresize(image, (28, 28))
+                    # image = rgb2gray(image)
+                    # image = np.expand_dims((image.astype('float') / 255.0) - 0.5, 0)
+                    ret_images.append(image)
+                    ret_labels.append(count-1)
+    print()
+
+    return np.array(ret_images), np.array(ret_labels), ret_class_names
 
 def unison_shuffled_copies(a, b):
     assert len(a) == len(b)
@@ -25,6 +66,6 @@ def split_data(images, labels):
     train_percent = 0.7
     count = math.floor(len(images)*train_percent)
     images, labels = unison_shuffled_copies(images, labels)
-    train_images, test_images = images[:count,:], images[count:,:]
+    train_images, test_images = images[:count], images[count:]
     train_labels, test_labels = labels[:count], labels[count:]
     return (train_images, train_labels), (test_images, test_labels)
--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import numpy as np
-from graph import *
 from ml import *
-from data import *
 import sys
-from sklearn.metrics import *
-
-print("Tensorflow version "+tf.__version__)
 
 if __name__ == "__main__":
     """
@@ -18,53 +12,4 @@ if __name__ == "__main__":
     visualize_arg = len(sys.argv) > 1 and (sys.argv[1].lower() == 'visualize')
     no_save_arg = not (len(sys.argv) > 2 and (sys.argv[2].lower() == 'no-save'))
 
-    train_images, train_labels, test_images, test_labels, class_names = load_data()
-    model = ml_model(train_images, train_labels, retrain_arg, no_save_arg)
-
-    test_loss, test_acc = model.evaluate(test_images, test_labels)
-    train_loss, train_acc = model.evaluate(train_images, train_labels)
-
-    predictions_test = model.predict(test_images)
-    predictions_train = model.predict(train_images)
-
-    pred_train = np.argmax(predictions_train, axis=1)
-    pred_test = np.argmax(predictions_test, axis=1)
-    actual_train = train_labels
-    actual_test = test_labels
-
-    cm_train = confusion_matrix(y_true=actual_train, y_pred=pred_train)
-    cm_test = confusion_matrix(y_true=actual_test, y_pred=pred_test)
-
-    accuracy_train = accuracy_score(actual_train, pred_train)
-    precision_train = precision_score(actual_train, pred_train, average='micro')
-    recall_train = recall_score(actual_train, pred_train, average='micro')
-    f1_train = f1_score(actual_train, pred_train, average='micro')
-
-    accuracy_test = accuracy_score(actual_test, pred_test)
-    precision_test = precision_score(actual_test, pred_test, average='micro')
-    recall_test = recall_score(actual_test, pred_test, average='micro')
-    f1_test = f1_score(actual_test, pred_test, average='micro')
-
-    print('\nTraining accuracy:\t' + str(accuracy_train))
-    print('Training precision:\t' + str(precision_train))
-    print('Training recall:\t' + str(recall_train))
-    print('Training f1:\t' + str(f1_train))
-
-    print('\nTesting accuracy:\t' + str(accuracy_test))
-    print('Testing precision:\t' + str(precision_test))
-    print('Testing recall:\t' + str(recall_test))
-    print('Testing f1:\t' + str(f1_test))
-
-    print('\nTraining Confusion Matrix:\n' + str(cm_train))
-    print('\nTesting Confusion Matrix:\n' + str(cm_test))
-
-    if visualize_arg:
-        # display test predictions
-        display_single_prediction(predictions_test, test_labels, test_images, class_names, 0)
-        display_single_prediction2(test_labels,class_names,model,test_images[0])
-        display_multiple_prediction(predictions_test, test_labels, test_images, class_names)
-
-        # display train predictions
-        display_single_prediction(predictions_train, train_labels, train_images, class_names, 0)
-        display_single_prediction2(train_labels,class_names,model,train_images[0])
-        display_multiple_prediction(predictions_train, train_labels, train_images, class_names)
+    run(not retrain_arg, no_save_arg, visualize_arg)
